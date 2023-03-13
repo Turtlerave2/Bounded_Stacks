c210f3874c698f4fec7a1c34c548da71f7c094f0
leoze <leoze@10.102.197.196>
2023-03-13 12:14:49 +0000

    major changes made, 2 constructers, 2 methods

--- bounded_Stack/src/main/java/teamarson/bounded_stack/Bounded_Stack.java
+++ bounded_Stack/src/main/java/teamarson/bounded_stack/Bounded_Stack.java
@@ -2,122 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package teamarson.bounded_stack;
-
-import java.util.EmptyStackException;
 
 /**
  *
  * @author leoze
  */
-public class Bounded_Stack {
+public class Bounded_Stack extends Stack {
 
-   private Node first;
-    private int size;
-   
+    private static final int DEFAULT_CAPACITY = 10;
+    private int capacity;
+
     public Bounded_Stack() {
-        first = null;
-        size = 0;
+        super();
+        capacity = DEFAULT_CAPACITY;
     }
 
-    public int size() {
-        return size;
+    public Bounded_Stack(int capacity) {
+        super();
+        this.capacity = capacity;
     }
 
-
-    public boolean push(String value) {
-        Node newNode = new Node(value);
-        
-        newNode.next = first;
-        first = newNode;
-        
-        size++;
-        return true;
-        
+    public boolean isFull() {
+        return size >= capacity;
     }
-    
-     public String get(int pos) {
 
-        String word = null;
-        int tracker = 0;
+    @Override
+    public boolean push(int value) {
 
-        if (pos >= size || pos < 0) {
-            throw new IndexOutOfBoundsException("Illegal position supplied");
+        if (isFull()) {
+            throw new StackFullException();
         }
-        Node current = first;
-        while (current != null) {
-
-            if (tracker == pos) {
-                word = current.data;
-            }
-            current = current.next;
-            tracker++;
-        }
-
-        return word;
+        return super.push(value);
 
     }
 
-    public int indexOf(String word) {
-
-        int count = 0;
-        if (word == null) {
-            throw new IllegalArgumentException("word is empty");
-        }
-
-        if (size == 0) {
-            throw new NullPointerException("notin here");
-        }
-
-        Node current = first;
-        while (current.next != null) {
-
-            if (current.data.contains(word)) {
-                return count;
-            }
-            count++;
-            current = current.next;
-        }
-
-        return -1;
-
-    }
-
-
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public Node Peek() {
-        if (isEmpty() == true) {
-            throw new EmptyStackException();
-        }
-        Node current = first;
-        return current;
-    }
-
-    public void clear() {
-        first = null;
-        size = 0;
-    }
-    
-    
-    
-      private static class Node {
-
-        private String data;
-        private Node next;
-
-        public Node(String data) {
-            this.data = data;
-            this.next = null;
-
-        }
-
-        public boolean hasNext() {
-            return next == null;
-        }
-
-    }
 }
